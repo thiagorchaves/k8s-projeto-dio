5c31f2854e414be89d68e5146a555a82e85aae58
Thiago Chaves <thiago.chaves@enjoei.com.br>
2024-02-26 22:11:53 -0300

    adicionando banco e imagens

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,5 +1,6 @@
 <?php
-$servername = "";
+
+$servername = "mysql-connect-db";
 $username = "root";
 $password = "Senha123";
 $database = "meubanco";
--- frontend/js.js
+++ frontend/js.js
@@ -1,16 +1,16 @@
 $("#button-blue").on("click", function() {
-    
+
     var txt_nome = $("#name").val();
     var txt_email = $("#email").val();
     var txt_comentario = $("#comment").val();
 
     $.ajax({
         url: "",
-        
+
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
         beforeSend: function() {
-        
+
             console.log("Tentando enviar os dados....");
 
         }
